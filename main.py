752771190a4a86e991f7780cfce79b27d3397e93
Sanoj-Omli <kumarisupriyasoni96@gmail.com>
2026-04-07 22:33:16 +0530

    learning FastAPI

---
+++ main.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+#from fastapi.responses import HTMLResponse
+
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Corey Schafer",
+        "title": "FastAPI is Awesome",
+        "content": "This framework is really easy to use and super fast.",
+        "date_posted": "April 20, 2025",
+    },
+    {
+        "id": 2,
+        "author": "Jane Doe",
+        "title": "Python is Great for Web Development",
+        "content": "Python is a great language for web development, and FastAPI makes it even better.",
+        "date_posted": "April 21, 2025",
+    },
+]
+
+@app.get("/", include_in_schema=False,name="home")
+@app.get("/posts", include_in_schema=False,name="posts")
+def home(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "home.html",
+        {"posts": posts, "title": "Home"},
+)
+ 
+@app.get("/posts/{post_id}", include_in_schema=False)
+def get_post(request: Request,post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            title = post["title"][:50]
+            return templates.TemplateResponse(
+        request,
+        "post.html",
+        {"posts": post, "title": "title"},
+)
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Post not Found")
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
+
+@app.get("/api/posts/{post_id}")
+def get_post(post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Post not Found")
